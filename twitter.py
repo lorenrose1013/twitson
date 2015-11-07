abceac2d71d0250d23ff43c2b2ce5023132c976f
Daisy <dbarbanel555@gmail.com>
2015-11-06 22:14:10 -0500

    Fully functional. Can return string of tweets made by a twitter user

---
+++ twitter.py
@@ -0,0 +1,56 @@
+import json, urllib2, urllib
+from base64 import b64encode
+
+
+#authentication keys
+apikey = "SOpevOakpUrEQhmvyUXjR3qQ6"
+apisecret = "ccK8U0k7VFuoo0mQ8IxVgrw7NMEFRdyvWfDqjxc70YWGYDEpCN"
+key = b64encode(apikey + ":" + apisecret)
+authurl = "https://api.twitter.com/oauth2/token/"
+
+#sends general request
+def apirequest(url, auth, post):
+    heads = {'Host':'api.twitter.com',
+             'User-Agent':'TwitsonLD',
+             'Authorization': auth}
+    data = None
+    if post:
+        data = {'grant_type':'client_credentials',
+                'Content-Type':'application/x-www-form-urlencoded;charset=UTF-8'}
+        data = urllib.urlencode(data)
+    req = urllib2.Request(url, data, headers = heads)
+    try:
+        return urllib2.urlopen(req).read()
+    except urllib2.HTTPError, error:
+        print error.read()
+
+
+#gets authorization token from twitter
+token = json.loads(apirequest(authurl, "Basic " + key, True))
+tokenauth = "Bearer " + token['access_token'] #used to authorize api requests
+
+
+#returns string of numtweets tweets written by user
+#does not include retweets
+#should this include replies or nah?
+def getUserTweets(user, numtweets):
+    uri = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=%s&count=%s"
+    url = uri%(user,numtweets)
+    results = json.loads(apirequest(url, tokenauth, False))
+    #pretty = json.dumps(results, sort_keys=True, indent=4)#for testing
+    tweets = ""
+    for post in results:
+        if post['retweeted'] == False:
+            tweets += "\n" + post['text']
+    return tweets
+
+#returns id(#) of user with the screen name user
+def getUserId(user):
+    uri = "https://api.twitter.com/1.1/users/lookup.json?screen_name=%s"
+    url = uri%(user)
+    results = json.loads(apirequest(url, tokenauth, False))
+    return results[0]['id']
+
+#Tests
+#print getUserTweets("twitterapi",10)
+#print getUserId("twitterapi")
