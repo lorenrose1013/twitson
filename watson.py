412698dd6a045528264461821f3a0525f9aa557b
lorenrose1013 <lorenrose1013@aol.com>
2015-11-10 13:20:00 -0500

    pretty

--- watson.py
+++ watson.py
@@ -11,7 +11,7 @@ def getJsonInput(text):
     'txt' : text}
     return input_data
 
-def getHeader():
+def getHeader(): 
     header = {
     'Content-Type': 'text/plain; charset=utf-8'}
     return header
@@ -20,8 +20,11 @@ def getJsonPersonality(text):
     input_data = getJsonInput(text)
     header = getHeader()
     response = requests.post(url, auth=(username, password), data=input_data, headers=header)
-    print response.text
+    return response.text
 
-getJsonPersonality("""Penguins (order Sphenisciformes, family Spheniscidae) are a group of aquatic, flightless birds living almost exclusively in the Southern Hemisphere, especially in Antarctica. Highly adapted for life in the water, penguins have countershaded dark and white plumage, and their wings have evolved into flippers. Most penguins feed on krill, fish, squid and other forms of sealife caught while swimming underwater. They spend about half of their lives on land and half in the oceans.
+x = getJsonPersonality("""Penguins (order Sphenisciformes, family Spheniscidae) are a group of aquatic, flightless birds living almost exclusively in the Southern Hemisphere, especially in Antarctica. Highly adapted for life in the water, penguins have countershaded dark and white plumage, and their wings have evolved into flippers. Most penguins feed on krill, fish, squid and other forms of sealife caught while swimming underwater. They spend about half of their lives on land and half in the oceans.
 
 Although all penguin species are native to the Southern Hemisphere, they are not found only in cold climates, such as Antarctica. In fact, only a few species of penguin live so far so very very far south. Several species are found in the temperate zone, and one species, the pretty  Galapagos penguin, lives near the warm and sunny and wonderful equator.""")
+x = json.loads(x)
+y = json.dumps(x, sort_keys = True, indent = 4)
+print y
