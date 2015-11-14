43bf41f48f929f8d98ae98e007ac95ac55aee8a9
Daisy <dbarbanel555@gmail.com>
2015-11-13 21:48:44 -0500

    reformatted

--- twitter.py
+++ twitter.py
@@ -2,12 +2,6 @@ import json, urllib2, urllib
 from base64 import b64encode
 
 
-#authentication keys
-apikey = "SOpevOakpUrEQhmvyUXjR3qQ6"
-apisecret = "ccK8U0k7VFuoo0mQ8IxVgrw7NMEFRdyvWfDqjxc70YWGYDEpCN"
-key = b64encode(apikey + ":" + apisecret)
-authurl = "https://api.twitter.com/oauth2/token/"
-
 def apirequest(url, auth, post):
     """
     params:
@@ -31,10 +25,21 @@ def apirequest(url, auth, post):
     except urllib2.HTTPError, error:
         return error.read()
 
-
-#gets authorization token from twitter
-token = json.loads(apirequest(authurl, "Basic " + key, True))
-tokenauth = "Bearer " + token['access_token'] #used to authorize api requests
+def getToken():
+    """
+    return:
+         string,
+         authentification token from twiter
+    """
+    #authentication keys
+    apikey = "SOpevOakpUrEQhmvyUXjR3qQ6"
+    apisecret = "ccK8U0k7VFuoo0mQ8IxVgrw7NMEFRdyvWfDqjxc70YWGYDEpCN"
+    key = b64encode(apikey + ":" + apisecret)
+    authurl = "https://api.twitter.com/oauth2/token/"
+    #requst authorization token
+    token = json.loads(apirequest(authurl, "Basic " + key, True))
+    tokenauth = "Bearer " + token['access_token'] #used to authorize api requests
+    return tokenauth
 
 def getUserTweets(user):
     """
@@ -45,19 +50,17 @@ def getUserTweets(user):
           returns most recent 200 tweets
           does not include retweets and replies
     """
-    uri = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=%s&count=%s"
-    url = uri%(user,200)
-    results = json.loads(apirequest(url, tokenauth, False))
+    uri = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=%s"
+    url = uri%(user)
+    results = json.loads(apirequest(url, getToken(), False))
     #pretty = json.dumps(results, sort_keys=True, indent=4)#for testing
     if 'errors' in results:
-        #maybe we can use the code key to handle specific errors?
-        return results['errors'][0]
+        error = results['errors'][0]
+        #raises error containg error message sent from twitter
+        #i.e. if the user doesn't exist
+        raise ValueError(error['message'])
     tweets = ""
     for post in results:
         if post['retweeted'] == False and post['in_reply_to_screen_name'] == None:
             tweets += "\n" + post['text']
     return tweets
-
-# #Tests
-#print getUserTweets("sdgsdgsdgi")
-#print getUserId("twitterapi")
