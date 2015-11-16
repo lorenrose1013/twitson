993721152810cb3ec70f01635566d341ba13e002
lorenrose1013 <lorenrose1013@aol.com>
2015-11-16 00:20:51 -0500

    comments and cleaning

--- utils.py
+++ utils.py
@@ -2,10 +2,24 @@ import watson, twitter
 
 
 def getPersonalityDict(twitter_handle):
+	"""
+	params: 
+		twitter_handle: string of twitter user to analyze
+	return:
+		python dictionary of Watson's Big 5 personality traits and their respective percentages
+	"""
 	raw_text = twitter.getUserTweets(twitter_handle)
 	if len(raw_text) < 150:
 		return {'error' : 'too few words'}
 	return watson.getPersonalityDict(raw_text)
 
 def getRandomHandle():
+	"""
+	params: 
+		None
+	return:
+		donald trump's twitter handle for analysis
+	future:
+		return random celebrity twitter handle for fun
+	"""
 	return "realDonaldTrump"
--- watson.py
+++ watson.py
@@ -6,17 +6,35 @@ username="57fefcad-0b03-49eb-901e-e4f78a69f563"
 password="BeXGe4N7PzCC"
 
 def getJsonInput(text):
+	"""
+	params: 
+		text: raw string of >= 150 words for analysis
+	return:
+		input_data for the api request 
+	"""
     input_data = {
     'sid' : 'ie-en-news',
     'txt' : text}
     return input_data
 
 def getHeader(): 
+	"""
+	params: 
+		None
+	return:
+		header for the api request 
+	"""
     header = {
     'Content-Type': 'text/plain; charset=utf-8'}
     return header
 
 def getPersonalityDict(text):
+	"""
+	params: 
+		text: raw string of >= 150 words for analysis
+	return:
+		ython dictionary of Watson's Big 5 personality traits and their respective percentages
+	"""
 	json = getJsonPersonality(text)
 	dict = {}
 	for i in json['tree']['children'][0]['children'][0]['children']:
@@ -24,27 +42,16 @@ def getPersonalityDict(text):
 	return dict
 
 def getJsonPersonality(text):
+	"""
+	params: 
+		text: raw string of >= 150 words for analysis
+	return:
+		raw json responce from IBM Watson's Personality Analyzer
+	"""
     input_data = getJsonInput(text)
     header = getHeader()
     response = requests.post(url, auth=(username, password), data=input_data, headers=header)
     response = json.loads(response.text)
     return response
-
-# text = """Penguins (order Sphenisciformes, family Spheniscidae) are a group of aquatic, flightless birds living almost exclusively in the Southern Hemisphere, especially in Antarctica. Highly adapted for life in the water, penguins have countershaded dark and white plumage, and their wings have evolved into flippers. Most penguins feed on krill, fish, squid and other forms of sealife caught while swimming underwater. They spend about half of their lives on land and half in the oceans.
-
-# Although all penguin species are native to the Southern Hemisphere, they are not found only in cold climates, such as Antarctica. In fact, only a few species of penguin live so far so very very far south. Several species are found in the temperate zone, and one species, the pretty  Galapagos penguin, lives near the warm and sunny and wonderful equator."""
-# x = getJsonPersonality(text) //save api calls
-# f = open("jsonDump.txt", "r")
-# x = json.loads(f.read())
-# f.close()
-# # # x = x['tree']['children'][0]['children'][0]['children'][2]
-# # # x['tree']['children'][0] is the big five 
-# # x = x['tree']['children'][0]['children'][0]['children'][4]
-# # print x.keys()
-# # print x['name']
-
-# z = getPersonalityDict(text)
-# y = json.dumps(x, sort_keys = True, indent = 4)
-# #print y
 
 
